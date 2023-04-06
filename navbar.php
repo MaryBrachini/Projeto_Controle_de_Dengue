4c0bc0dbb3c9be27a76209870f95301367f11296
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-04-06 09:09:19 -0300

    06/047/2023

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+
+	<a class="navbar-brand" href="index.php">logo</a>
+
+	<div class="collapse navbar-collapse">
+
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Inicio</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Formulário</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Gráfico</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Relátorios</a>
+			</li>
+		</ul>
+	</div>
+</nav>
