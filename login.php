ef1c51eba37507f68efbf527cd7a208c96fbc4f4
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-04-08 12:16:42 -0300

    08/04/2023

--- login.php
+++ login.php
@@ -1,23 +1,40 @@
 <?php
+session_start();
+if (isset($_SESSION["user"]) && !empty($_SESSION["user"])) {
+	header("Location: inicio.php");
+}
+
 require "header.php";
 ?>
 <style>
 	body {
-		height: 100%;
 		align-items: center;
 		background-image: url('img/background.gif');
 		backdrop-filter: blur(10px);
 		background-position: center;
 		background-repeat: no-repeat;
 		background-size: cover;
+		height: 100vh;
+		padding-top: 5%;
+		position: relative;
+	}
 
+	body::before {
+		content: "";
+		position: absolute;
+		top: 0px;
+		right: 0px;
+		bottom: 0px;
+		left: 0px;
+		background-color: rgba(100, 100, 100, 0.3);
 	}
 
 	.row {
+		position: relative;
 		margin-left: auto;
 		margin-right: auto;
 		background-color: white;
-		border-radius: 10px;
+		border-radius: 15px;
 		height: 75%;
 	}
 
@@ -26,7 +43,8 @@ require "header.php";
 		background-repeat: no-repeat;
 		background-size: cover;
 		background-image: url('img/background.gif');
-
+		padding-top: 10%;
+		border-radius: 15px 0px 0px 15px;
 	}
 
 	.form-floating {
@@ -36,6 +54,14 @@ require "header.php";
 	form {
 		padding: 5%;
 	}
+
+	.txt {
+		color: white;
+		text-shadow: 2px 2px 5px black;
+		font-weight: bold;
+		align-items: center;
+
+	}
 </style>
 
 <body class="text-center bg-img">
@@ -44,12 +70,15 @@ require "header.php";
 		<div class="row">
 
 			<div class="div-img col-6">
+				<h1 class="txt" style="font-size:50px;">BEM-VINDO !</h1>
+				<p class="txt">Faça o login para acessar ao nosso site !</p>
 			</div>
 
 			<div class="col-6">
 				<main class="form-login w-100 m-auto">
-					<form action="">
-						<img class="mb-4" src="img/user.png" alt="" width="75" height="75">
+
+					<form method="POST" action="" class="form-signin">
+						<img class="mb-4" src="img/user.png" alt="" width="100" height="100">
 
 						<div class="form-floating">
 							<input type="name" class="form-control" id="floatingInput">
@@ -68,3 +97,6 @@ require "header.php";
 		</div>
 
 	</div>
+</body>
+
+</html>
