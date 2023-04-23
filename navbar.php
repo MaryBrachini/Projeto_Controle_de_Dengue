33906a035996dd934d2eff7e2be5e451aad107a7
Mary Brachini <92119838+MaryBrachini@users.noreply.github.com>
2023-04-23 12:13:20 -0300

    23/04/2023

--- navbar.php
+++ navbar.php
@@ -2,21 +2,32 @@
 
 	<a class="navbar-brand" href="index.php">logo</a>
 
-	<div class="collapse navbar-collapse">
+	<ul class="navbar-nav">
+		<li class="nav-item active">
+			<a class="nav-link" href="inicio.php">Inicio</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Formulário</a>
+		</li>
+		<?php
+		/* if (isset($_SESSION["usuario"]) || !empty($_SESSION["usuario"])) { */
+		?>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Gráfico</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="relatorio.php">Relátorios</a>
+		</li>
+		<li class="nav-item">
+			<a href="sair.php" class="nav-link">Sair</a>
+		</li>
+	</ul>
 
-		<ul class="navbar-nav">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Inicio</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Formulário</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Gráfico</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Relátorios</a>
-			</li>
-		</ul>
-	</div>
+	<?php /* } else { */ ?>
+	<ul class="nav navbar-nav navbar-right">
+		<li class="nav-text">
+			<a class="nav-link" href="login.php">Login</a>
+		</li>
+	</ul>
+	<?php /* } */ ?>
 </nav>
